3a2cf4b9f16bfe042364b4b6ecc699ebcd7a5cb6
Efrain Loya <soporte@tannerherramientas.com>
2017-08-28 23:27:27 +0000

    [REF] openacademy: Add active field, datetime_eg, and set default fields start_date, add compute field taken_seats using @api.one and @api.depends

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models
+from openerp import api, fields, models
 
 class Session(models.Model):
-   _name = 'openacademy.session'
-
-   name = fields.Char(required=True)
-   start_date = fields.Date()
-   duration = fields.Float(digits=(6, 2), help="Duration in days")
-   seats = fields.Integer(string="Number of seats")
-   instructor_id = fields.Many2one('res.partner', string="Instructor")
-   course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
-   attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    _name = 'openacademy.session'
+   
+    name = fields.Char(required=True)
+    start_date = fields.Date(default=fields.Date.today)
+    datetime_eg = fields.Datetime(default=fields.Datetime.now)
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor", 
+                                    domain=['|',('instructor','=', True),('category_id.name','ilike','Teacher')])
+    course_id = fields.Many2one('openacademy.course',
+         ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active = fields.Boolean(default=True)
+    @api.one
+    @api.depends('seats','attendee_ids')
+    def _taken_seats(self):
+        if not self.seats:
+            self.taken_seats = 0.0
+        else:
+            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
